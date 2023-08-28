532b89c3067950132affe6353b857c01907d541c
raysz999 <135997578+raysz999@users.noreply.github.com>
2023-08-28 22:33:08 +0700

    Add files via upload

---
+++ src/function.js
@@ -0,0 +1,6 @@
+function redirWhatsapp() {
+  window.location.href = "https://wa.me/+6282115067208";
+}
+function redirInstagram() {
+  window.location.href = "https://www.instagram.com/huffman.y.g/";
+}
